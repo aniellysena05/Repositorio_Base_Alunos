1e261c1cf9258d683efa4b4e325da34db4350aca
aniellysena05 <Silvaanielly02@gmail.com>
2025-09-10 14:24:13 -0300

    Add files via upload

---
+++ Modulo 1/Listas/lista_de_tarefas.py
@@ -0,0 +1,23 @@
+def menu():
+    print ('-'*40)
+    print ('========MENU DE TAREFAS========')
+    print('(1) Mostrar tarefa')
+    print('(2) Adicionar tarefa')
+    print('(3) Remover tarefa')
+    print ('(4) Sair')
+    print ('-'*40)
+    opcoes=input ('Escolha uma opcao:')
+    if opcoes ==('1'):
+        print('opcao 1')
+    elif opcoes == ('2'):
+        print('opcao 2')
+    elif opcoes == ('3'):
+         print('opcao 3')
+    elif opcoes == ('4'):
+         print('opcao 4')
+    else:
+        print('Opcao errada')
+
+
+menu()
+    
